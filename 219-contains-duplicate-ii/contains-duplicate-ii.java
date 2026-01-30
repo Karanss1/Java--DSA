cb8ffdbc8724a9dd4e01ed7d1e1ac6aa2102e5ed
SS Karan <karanss.m05@gmail.com>
2026-01-30 15:44:12 +0530

    Time: 22 ms (92.56%) | Memory: 90.7 MB (36.30%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            // If map already has the number and the difference between indices is <= k
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            // Update the latest index of the current number
+            map.put(nums[i], i);
+        }
+        // If no such pair found, return false
+        return false;
+    }
+}
