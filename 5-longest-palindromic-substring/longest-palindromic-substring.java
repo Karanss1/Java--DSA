0b167130ce60ec13d4b55169a8c4564f7774da14
SS Karan <karanss.m05@gmail.com>
2026-02-19 01:46:29 +0530

    Time: 18 ms (48.27%) | Memory: 43.6 MB (70.45%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,29 @@
+// Solution: Iterate the string, for each character, 
+// try to expand left and right to get the longest palindromic substring
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.trim().equals("")) {
+            return s;
+        }
+        int len = s.length();
+        int begin = 0;
+        int maxLen = 0;
+        for (int i = 0; i < len - maxLen / 2; i++) {
+            int j = i;
+            int k = i;
+            while (k < len - 1 && s.charAt(k) == s.charAt(k + 1)) { 
+                k++;
+            }
+            while (j > 0 && k < len - 1 && s.charAt(j-1) == s.charAt(k+1)) { // Expand both left and right
+                j--;
+                k++;
+            }
+            int newLen = k - j + 1;
+            if (newLen > maxLen) {
+                begin = j;
+                maxLen = newLen;
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+}
