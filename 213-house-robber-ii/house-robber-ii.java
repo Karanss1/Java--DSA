dc7961f2d8807a8e8372dcfef9666e2952005cc0
SS Karan <karanss.m05@gmail.com>
2026-06-24 02:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (89.80%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -5,12 +5,12 @@ class Solution {
         if (n == 1) return nums[0];
 
         return Math.max(
-            robLinear(nums, 0, n - 2),
-            robLinear(nums, 1, n - 1)
+            rob(nums, 0, n - 2),
+            rob(nums, 1, n - 1)
         );
     }
 
-    private int robLinear(int[] nums, int start, int end) {
+    private int rob(int[] nums, int start, int end) {
         int prev2 = 0;
         int prev1 = 0;
 
